82119e511d444e86ebeb24f8d04a9053ee91a821
ali-Sattarov <eliseddarov2022@gmail.com>
2024-10-30 21:31:52 +0400

    added task swap x ant y

--- src/lesson2_2/swap_x_and_y.java
+++ src/lesson2_2/swap_x_and_y.java
@@ -2,6 +2,14 @@ package lesson2_2;
 
 public class swap_x_and_y {
     public static void main(String[] args) {
+      int x=10;
+      int y=8;
+      x=x+y;//18
+      y=x-y;//10
+      x=x-y;//8
+        System.out.println(y);
+
+
 
     }
 }
